1f2007f6734ef46579c333ad93de0538be3bb785
Aurélien St <aurelien.strimelle@bstorm.be>
2024-12-18 15:46:32 +0100

    Version 1

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,90 @@
 "use strict";
+// Les éléments DOM
+const container = document.getElementById('container');
+// Variable
+let mysteryWord;
+let letterWord;
+let nbLife;
+//TODO Affiche le dessin
+// Méthodes
+async function initialisation() {
+    mysteryWord = await getRandomWord();
+    letterWord = getLettersOfMystery();
+    nbLife = 6;
+    generateMystery(mysteryWord);
+    generateKeyboard();
+}
+initialisation();
+async function getRandomWord() {
+    //TODO Consommer une WebAPI pour obtenir un mot aleatoire
+    return 'Éléphant';
+}
+;
+function getLettersOfMystery() {
+    const result = new Set(getWordForCompare().replaceAll(/[ -]/g, ''));
+    return [...result];
+}
+function generateMystery(word) {
+    const baliseText = document.createElement('p');
+    baliseText.id = "mystery-text";
+    for (const letter of word) {
+        const baliseLetter = document.createElement('span');
+        baliseLetter.textContent = (letter !== ' ') ? '_' : letter;
+        baliseText.append(baliseLetter);
+    }
+    container.append(baliseText);
+}
+function generateKeyboard() {
+    const keyboard = document.createElement('div');
+    keyboard.id = "keyboard";
+    const letterStart = 'A'.charCodeAt(0);
+    const letterEnd = 'Z'.charCodeAt(0);
+    for (let code = letterStart; code <= letterEnd; code++) {
+        // console.log(code, String.fromCharCode(code));
+        const letter = String.fromCharCode(code);
+        const buttonLetter = document.createElement('button');
+        buttonLetter.textContent = letter;
+        buttonLetter.addEventListener('click', handleKeyboardClick);
+        keyboard.append(buttonLetter);
+    }
+    container.append(keyboard);
+}
+function getWordForCompare() {
+    return mysteryWord.toUpperCase() // Majuscule
+        .normalize("NFD") // Normalise (Caractere acentué)
+        .replace(/\p{Diacritic}/gu, ""); // Supprime les "Diacritique"
+}
+function handleKeyboardClick(event) {
+    const button = event.target;
+    button.disabled = true;
+    const letter = button.textContent ?? '';
+    if (letterWord.includes(letter)) {
+        revealLetter(letter);
+        letterWord = letterWord.filter(l => l !== letter);
+        if (letterWord.length === 0) {
+            displayWin();
+        }
+    }
+    else {
+        nbLife--;
+        if (nbLife === 0) {
+            displayLose();
+        }
+    }
+}
+function revealLetter(letter) {
+    const mysteryText = document.getElementById('mystery-text');
+    console.log(mysteryText.children);
+    Array.from(getWordForCompare()).forEach((wordLetter, index) => {
+        if (wordLetter === letter) {
+            // console.log(wordLetter, index);
+            mysteryText.children[index].textContent = mysteryWord[index];
+        }
+    });
+}
+function displayWin() {
+    alert('Bravo');
+}
+function displayLose() {
+    alert('Perdu');
+}
---
+++ src/script.ts
@@ -0,0 +1,114 @@
+// Les éléments DOM
+const container = document.getElementById('container') as HTMLDivElement;
+
+// Variable
+let mysteryWord : string;
+let letterWord : string[];
+let nbLife : number;
+
+//TODO Affiche le dessin
+
+// Méthodes
+async function initialisation() : Promise<void> {
+    mysteryWord = await getRandomWord();
+    letterWord = getLettersOfMystery();
+    nbLife = 6;
+
+    generateMystery(mysteryWord);
+    generateKeyboard();
+}
+initialisation();
+
+async function getRandomWord() : Promise<string> {
+    //TODO Consommer une WebAPI pour obtenir un mot aleatoire
+    return 'Éléphant';
+};
+
+function getLettersOfMystery() : string[] {
+    const result = new Set(getWordForCompare().replaceAll(/[ -]/g, ''))
+    return [...result];
+}
+
+function generateMystery(word : string) : void {
+    const baliseText = document.createElement('p');
+    baliseText.id = "mystery-text"
+    
+    for(const letter of word) {
+        const baliseLetter = document.createElement('span');
+        baliseLetter.textContent = (letter !== ' ') ? '_' : letter;
+        
+        baliseText.append(baliseLetter);
+    }
+
+    container.append(baliseText);
+}
+
+function generateKeyboard() : void {
+    const keyboard = document.createElement('div');
+    keyboard.id= "keyboard";
+
+    const letterStart = 'A'.charCodeAt(0);
+    const letterEnd = 'Z'.charCodeAt(0);
+
+    for(let code = letterStart; code <= letterEnd; code++) {
+        // console.log(code, String.fromCharCode(code));
+        const letter = String.fromCharCode(code);
+
+        const buttonLetter = document.createElement('button');
+        buttonLetter.textContent = letter;
+        buttonLetter.addEventListener('click', handleKeyboardClick);
+
+        keyboard.append(buttonLetter);
+    }
+
+    container.append(keyboard)
+}
+
+function getWordForCompare() : string {
+    return mysteryWord.toUpperCase()                    // Majuscule
+                      .normalize("NFD")                 // Normalise (Caractere acentué)
+                      .replace(/\p{Diacritic}/gu, "");  // Supprime les "Diacritique"
+}
+
+function handleKeyboardClick(event: Event) : void {
+    const button = event.target as HTMLButtonElement; 
+    button.disabled = true;
+
+    const letter = button.textContent ?? '';
+
+    if(letterWord.includes(letter)) {
+        revealLetter(letter);
+        letterWord = letterWord.filter(l => l !== letter);
+
+        if(letterWord.length === 0) {
+            displayWin();
+        }
+    }
+    else {
+        nbLife--;
+
+        if(nbLife === 0) {
+            displayLose();
+        }
+    }
+}
+
+function revealLetter(letter: string) {
+    const mysteryText = document.getElementById('mystery-text') as HTMLDivElement;
+    console.log(mysteryText.children);
+    
+    Array.from(getWordForCompare()).forEach((wordLetter, index) => {
+        if(wordLetter === letter) {
+            // console.log(wordLetter, index);
+            mysteryText.children[index].textContent = mysteryWord[index];
+        }
+    });
+}
+
+function displayWin() : void {
+    alert('Bravo');
+}
+
+function displayLose() : void {
+    alert('Perdu');
+}
